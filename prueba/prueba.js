8020fdd284cb2c6e5d6628e80b731b7a1689b2fb
antomate22 <blascoantonella02@gmail.com>
2024-06-19 20:18:10 -0300

    bramcess

--- prueba/prueba.js
+++ prueba/prueba.js
@@ -1,4 +1,27 @@
 let listaD = document.createElement("ul");
 document.body.appendChild(listaD);
+
 let futer = document.getElementById("piepagina");
 futer.appendChild(listaD);
+
+let salto = document.createElement("hr")
+
+let itemlista1 = document.createElement("li");
+itemlista1.textContent = "achusssssss!";
+
+let item3 = document.createElement("li");
+itemlista1.textContent = "si mami!";
+
+let itemlista2 = document.createElement("li");
+itemlista2.textContent = "salud!";
+listaD.appendChild(itemlista1);
+listaD.appendChild(itemlista2);
+listaD.appendChild(salto);
+listaD.appendChild(item3);
+item3.style.fontfamily = "Impact";
+futer.style.padding = "1rem"
+futer.style.boxShadow = "0 0 15px -5 black";
+
+
+ let hacheTres = document.querySelector("h3")
+ hacheTres.classList.add("subrayado")
